bbbcdb5a2a380f5011a8f32cd46b5f96e4feae6e
Adrien Koumgang T. <64235641+adrienKoumgangT@users.noreply.github.com>
2026-06-12 16:15:43 +0200

    update load data, preprocess and encode features

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,17 +2,62 @@ import os
 
 import pandas as pd
 
-raw_data_path = os.path.join(os.getcwd(), 'data', 'raw')
-processed_data_path = os.path.join(os.getcwd(), 'data', 'processed')
+raw_data_path = os.path.join(os.getcwd(), '..', 'data', 'raw')
+processed_data_path = os.path.join(os.getcwd(), '..', 'data', 'processed')
 
-def load_data_raw(filename: str = 'student_dropout_academic_success.csv'):
-    df = pd.read_csv(os.path.join(raw_data_path, filename))
-    return df
 
-def load_data_processed(filename: str = 'student_dropout_academic_success.csv'):
-    df = pd.read_csv(os.path.join(processed_data_path, filename))
-    return df
+def load_raw_data(filename: str) -> pd.DataFrame:
+    """
+    Load raw dataset from CSV file.
 
-def save_data_processed(df: pd.DataFrame, filename: str = 'student_dropout_academic_success.csv'):
-    df.to_csv(os.path.join(processed_data_path, filename), index=False)
+    Parameters:
+    -----------
+    filepath : str
+        Path to the raw CSV file
+
+    Returns:
+    --------
+    pd.DataFrame : Raw dataframe
+    """
+    filepath = os.path.join(raw_data_path, filename)
+    print(f"Loading data from: {filepath}")
+
+    try:
+
+        df = pd.read_csv(filepath)
+        print(f"Dataset loaded: {df.shape[0]} rows, {df.shape[1]} columns")
+        return df
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Dataset not found at {filepath}. "
+                                f"Please place your CSV file in the data/raw/ directory.")
+    except Exception as e:
+        raise Exception(f"Error loading data: {str(e)}")
+
+
+def load_processed_data(filename: str) -> pd.DataFrame:
+    """
+    Load raw dataset from CSV file.
+
+    Parameters:
+    -----------
+    filepath : str
+        Path to the raw CSV file
+
+    Returns:
+    --------
+    pd.DataFrame : Raw dataframe
+    """
+    filepath = os.path.join(processed_data_path, filename)
+    print(f"Loading data from: {filepath}")
+
+    try:
+
+        df = pd.read_csv(filepath)
+        print(f"Dataset loaded: {df.shape[0]} rows, {df.shape[1]} columns")
+        return df
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Dataset not found at {filepath}. "
+                                f"Please place your CSV file in the data/processed/ directory.")
+    except Exception as e:
+        raise Exception(f"Error loading data: {str(e)}")
 
